1511ae6d338d9472001c9decd8740e5eb7bdae23
melaherediainw <melanie.heredia@inworx.com>
2024-11-27 20:47:24 +0000

    Ejecicios resueltos hasta el 11 inclusive

--- 01_restaurant/challenges/01_09_createreservation_ch.sql
+++ 01_restaurant/challenges/01_09_createreservation_ch.sql
@@ -4,3 +4,24 @@
 -- Use the following information:
 -- Sam McAdams (smac@kinetecoinc.com), for 5 people
 -- on August 12, 2022 at 6PM (18:00)
+
+SELECT CustomerId FROM Customers WHERE FirstName = 'Sam' AND LastName = 'McAdams' AND Email = 'smac@kinetecoinc.com';
+
+INSERT INTO Customers
+(FirstName,LastName,Email, Phone)
+VALUES ('Sam','McAdams','smac@kinetecoinc.com','(555) 555-1232');
+
+INSERT INTO Reservations
+(
+  CustomerId,
+  Date,
+  PartySize
+)
+VALUES 
+(
+  (SELECT CustomerId FROM Customers WHERE FirstName = 'Sam' AND LastName = 'McAdams' AND Email = 'smac@kinetecoinc.com'),
+  '2022-08-12 18:00:00',
+  5
+);
+
+SELECT * FROM Reservations ORDER BY ReservationID DESC
